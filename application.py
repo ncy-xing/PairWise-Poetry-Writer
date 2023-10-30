3b032f473b3e45dd497521d4e25f9fc5525ad0ab
Nancy Xing <92757073+ncy-xing@users.noreply.github.com>
2023-10-30 16:46:39 -0400

    generate n words from midpoint

--- application.py
+++ application.py
@@ -4,8 +4,19 @@ Assignment: CSCI 3725 M7
 Date: 10-2023
 """
 from src.WordGenerator import WordGenerator
+import sys
 
 if __name__ == '__main__':
+    # Test word generation
+    if len(sys.argv) < 3:
+        print("Usage: word1 word2 num_results")
+        sys.exit()
+
     print("Started Generator")
-    word_generator = WordGenerator("swow_associative_embeddings.csv")
+    word_generator = WordGenerator("swow_associative_embeddings.csv", "vocab.csv")
     print(word_generator)
+
+    w1 = sys.argv[1]
+    w2 = sys.argv[2]
+    closest_words = word_generator.midpoint_scores(w1, w2, 10)
+    print(f"midpoint words from '{w1}' and '{w2}': {closest_words}")
--- src/WordGenerator.py
+++ src/WordGenerator.py
@@ -3,8 +3,12 @@ Nancy Xing
 Assignment: CSCI 3725 M7
 Date: 10-2023
 """
+from typing import *
 import pandas as pd
+import numpy as np
+import scipy
 import os
+import sys
 
 
 class WordGenerator():
@@ -13,17 +17,44 @@ class WordGenerator():
     words from the database based on semantic distance from given inspiring words.
     """
 
-    def __init__(self, data_file : str) -> None:
+    def __init__(self, data_file : str, vocab_file : str) -> None:
         """
         Initialize word embedding data.
         
         data_file -- name of csv word embedding data file stored in data folder. 
+        vocab_file -- name of csv containing all words in same order as data file.
         """
         print("Initializing Word Generator...")
         self.data_file = data_file
+        # TODO refactor 
         self.embeddings = pd.read_csv(os.path.join("data", data_file)).transpose().values
+        self.vocab = list(pd.read_csv(os.path.join("data", vocab_file)).Word)
         print("Initialized.")
     
+    def midpoint_scores(self, word1 : str, word2 : str, n_results : List[int]) -> List[str]: 
+        """
+        Computes the midpoint between two words. Finds and returns the words in the vocab set 
+        with the lowest cosine distance from the midpoint.
+
+        word1, word2 -- word pair
+        n_results -- number of midpoint words to return 
+        """
+        # Retrieve word pair vectors 
+        w1_vector = self.embeddings[self.vocab.index(word1)]
+        w2_vector = self.embeddings[self.vocab.index(word2)]
+        midpoint = (w1_vector + w2_vector)/2
+
+        # Compute midpoint score and word distances from score
+        midpoint = midpoint.reshape((1, self.embeddings.shape[1]))
+        similarities = 1 - scipy.spatial.distance.cdist(midpoint, self.embeddings, 'cosine')
+
+        # Find closest words
+        sim_array = np.array(similarities)
+        sim_array_sorted = np.argsort(-sim_array).flatten() # gives sorted indices
+        sim_array = np.flip(np.sort(sim_array)).flatten()
+        closest_words = np.array([self.vocab[i] for i in sim_array_sorted])
+        return closest_words[:n_results]
+
     def __str__(self) -> str:
         """Returns string of location and size of generator's data source."""
         serialize = "WordGenerator" + f"\ndata source = '{self.data_file}'" + \
