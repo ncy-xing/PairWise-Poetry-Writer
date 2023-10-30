0d5cf590ae8c6fc5b3c67a1954e5f63a650ab15f
Nancy Xing <92757073+ncy-xing@users.noreply.github.com>
2023-10-30 15:38:23 -0400

    word gen reads in csv embeddings

--- application.py
+++ application.py
@@ -3,7 +3,9 @@ Nancy Xing
 Assignment: CSCI 3725 M7
 Date: 10-2023
 """
-from src import *
+from src.WordGenerator import WordGenerator
 
 if __name__ == '__main__':
     print("Started Generator")
+    word_generator = WordGenerator("swow_associative_embeddings.csv")
+    print(word_generator)
--- src/WordGenerator.py
+++ src/WordGenerator.py
@@ -3,8 +3,30 @@ Nancy Xing
 Assignment: CSCI 3725 M7
 Date: 10-2023
 """
+import pandas as pd
+import os
+
 
 class WordGenerator():
-    def __init__(self) -> None:
-        pass
+    """
+    WordGenerator decodes embeddings from a given database of words and generates
+    words from the database based on semantic distance from given inspiring words.
+    """
+
+    def __init__(self, data_file : str) -> None:
+        """
+        Initialize word embedding data.
+        
+        data_file -- name of csv word embedding data file stored in data folder. 
+        """
+        print("Initializing Word Generator...")
+        self.data_file = data_file
+        self.embeddings = pd.read_csv(os.path.join("data", data_file)).transpose().values
+        print("Initialized.")
+    
+    def __str__(self) -> str:
+        """Returns string of location and size of generator's data source."""
+        serialize = "WordGenerator" + f"\ndata source = '{self.data_file}'" + \
+            f"\nsize = {len(self.embeddings)} words"
+        return serialize
     
